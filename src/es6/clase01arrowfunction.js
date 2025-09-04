13713efd524036f41cb680fdd893ff4872329b35
Jonatan Lisama <jtlisama21@gmail.com>
2025-09-03 21:01:11 -0600

    Clase arrow function

---
+++ src/es6/clase01arrowfunction.js
@@ -0,0 +1,19 @@
+//Funcion Tradicional
+
+function square(num) {
+    return num + num ;
+};
+
+square();
+
+//Arrow Function
+
+//Return explicito
+const square = (num) => {
+    return num * num;
+}
+
+//Retun implicito
+
+const square = num => num * num;
+
