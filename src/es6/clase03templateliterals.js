3a49b452e12131f05d38c23447d7e7c817df4bae
Jonatan Lisama <jtlisama21@gmail.com>
2025-09-05 22:31:23 -0600

    Template literals multiline

---
+++ src/es6/clase03templateliterals.js
@@ -0,0 +1,19 @@
+let hello = 'Hello';
+let world = 'Word';
+
+let myPhrase = hello + ' ' + world + '!';//Concatenar variables antes de ecmascript
+console.log(myPhrase);
+
+//Template Literals
+
+let myPhraseTwo = `${hello} ${world}!!`;
+console.log(myPhraseTwo);
+
+//Multi-line string
+
+let lorem = `Esto es una frase epica
+            con un salto de linia epico
+`;
+
+console.log(lorem);
+
