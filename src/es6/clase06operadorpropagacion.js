adcbbd24f7fb4f76d8f4d7366027b5a731339999
Jonatan Lisama <jtlisama21@gmail.com>
2025-09-06 21:19:13 -0600

    spread operator reset function

---
+++ src/es6/clase06operadorpropagacion.js
@@ -0,0 +1,23 @@
+//spread operator
+//podemos unir varios objetos o variables a una nueva variable
+
+let person = {
+    name: 'oscar',
+    age: 30
+};
+
+let country = 'MX';
+
+let data = {...person,country};
+console.log(data);
+
+//rest
+
+function sum(sum, ...values){
+    console.log(values);
+    console.log(num + values[0]);
+    return num + values[0];
+};
+
+
+sum(1,1,2,3)
