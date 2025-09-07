36d5656171fe258f104c185ff8918f748154a97e
Jonatan Lisama <jtlisama21@gmail.com>
2025-09-06 20:51:10 -0600

    Destructuracion arrays object

---
+++ src/es6/clase05restspread.js
@@ -0,0 +1,15 @@
+//Arrays destructuring
+
+let fruits = ['Apple','Uva'];
+let [a,b] = fruits;
+console.log(a,b);
+
+//Object destructuring
+
+let user = {
+    username: 'oscar',
+    age: 34
+};
+
+let {username,age} = user;
+console.log(username,age);
