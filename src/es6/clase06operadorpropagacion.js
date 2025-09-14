7c9f55da1e6d776abbea085abfbfec1f5ec9b8f6
Jonatan Lisama <jtlisama21@gmail.com>
2025-09-13 20:33:51 -0600

    Correcion funcion spreadoperator

--- src/es6/clase06operadorpropagacion.js
+++ src/es6/clase06operadorpropagacion.js
@@ -13,7 +13,7 @@ console.log(data);
 
 //rest
 
-function sum(sum, ...values){
+function sum(num, ...values){
     console.log(values);
     console.log(num + values[0]);
     return num + values[0];
